579b5d8514790a1373ca03571e2511ecc99d6b9e
Alan Barnett <alanbarnett328@gmail.com>
2020-01-26 09:57:43 -0800

    chore(comment): add comment to IMonitorDisplay.hpp

--- IMonitorDisplay.hpp
+++ IMonitorDisplay.hpp
@@ -1,19 +1,26 @@
 #ifndef IMONITORDISPLAY_HPP
-#define IMONITORDISPLAY_HPP
-#include "IMonitorModule.hpp"
-#include "MultiStrMonitorModule.hpp"
-#include "ChartMonitorModule.hpp"
-#include <string>
+# define IMONITORDISPLAY_HPP
+
+# include "IMonitorModule.hpp"
+# include "MultiStrMonitorModule.hpp"
+# include "ChartMonitorModule.hpp"
+# include <string>
+
+/*
+** Nothing is implemented here because this class serves to define the
+** interface. It is inherited by AMonitorDisplay, which contains the base
+** definition of these things.
+*/
 
 class IMonitorDisplay
 {
-public:
-    virtual void addMonitor(IMonitorModule *) = 0;
-    virtual void removeMonitor(const std::string &name) = 0;
-    virtual void run() = 0;
-    virtual void display(MultiStrMonitorModule *) = 0;
-    virtual void display(ChartMonitorModule<float>*) = 0;
-    virtual ~IMonitorDisplay() {};
+	public:
+		virtual void addMonitor(IMonitorModule *) = 0;
+		virtual void removeMonitor(const std::string &name) = 0;
+		virtual void run() = 0;
+		virtual void display(MultiStrMonitorModule *) = 0;
+		virtual void display(ChartMonitorModule<float>*) = 0;
+		virtual ~IMonitorDisplay() {};
 };
 
 #endif
