282e805f69f07ee4c095a8b093645d5586dcd9a2
Svetlana Nechaeva <snechaev@student.42.us.org>
2020-01-26 20:10:10 -0800

    fix RAM

--- RAMMonitorModule.cpp
+++ RAMMonitorModule.cpp
@@ -12,18 +12,7 @@
 
 RAMMonitorModule::RAMMonitorModule() : MultiStrMonitorModule("RAM info:")
 {
-    #ifdef __linux__
-        struct sysinfo info;
-        sysinfo(&info);
 
-        _strings.push_back(std::string("Total RAM: ") + toStr(info.totalram / (1024 * 1024)) + "M");
-        _strings.push_back(std::string("Free RAM: ") + toStr(info.freeram / (1024 * 1024)) + "M");
-
-    #elif defined(__APPLE__)
-        _strings.push_back(std::string("Total RAM: ") + toStr(getRamSize()) + "M");
-        _strings.push_back(std::string("Free RAM: ") + toStr(getFreeMem()) + "M");
-
-    #endif
 }
 RAMMonitorModule::~RAMMonitorModule()
 {
@@ -31,7 +20,20 @@ RAMMonitorModule::~RAMMonitorModule()
 
 void RAMMonitorModule::stat()
 {
+        _strings.clear();
+    #ifdef __linux__
+    struct sysinfo info;
+    sysinfo(&info);
+
+    _strings.push_back(std::string("Total RAM: ") + toStr(info.totalram / (1024 * 1024)) + "M");
+    _strings.push_back(std::string("Free RAM: ") + toStr(info.freeram / (1024 * 1024)) + "M");
+
+    #elif defined(__APPLE__)
+    _strings.push_back(std::string("Total RAM: ") + toStr(getRamSize()) + "M");
+    _strings.push_back(std::string("Free RAM: ") + toStr(getFreeMem()) + "M");
+     #endif
 }
+
 void RAMMonitorModule::beDisplayed(IMonitorDisplay *d)
 {
     d->display(this);
